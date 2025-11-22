ba7d8de560511aa87a08aba46b8a415915cb044a
superrk <superrk@users.noreply.github.com>
2025-11-22 06:15:54 +0000

    deploy: 791110dc59095f4ef003a405a392a66a0c9764d9

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lovemeng/sw.js', { scope: '/lovemeng/' })})}
